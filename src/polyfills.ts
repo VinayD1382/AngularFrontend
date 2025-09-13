4f6e7883a9ef8a4ab50a2d185a9954f7be160777
Vinay D <114236808+VinayD1382@users.noreply.github.com>
2025-09-13 15:13:46 +0530

    Create polyfills.ts

---
+++ src/polyfills.ts
@@ -0,0 +1,21 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * This setup is for browsers that don't support required features by default.
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/** IE11 requires the following polyfills */
+// import 'core-js/es/array';
+// import 'core-js/es/object';
+// import 'core-js/es/symbol';
+// import 'core-js/es/function';
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
